533a94d207f4faad4b5f28f7b35d45d530674a13
Joy <kokimalinda@gmail.com>
2023-03-16 15:24:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+multiply;
 
-// Write your code here
+let random = Math.floor(Math.random()*10);
+random;
+
+let num3 = 39;
+let num4 = 5;
+let mod = num3%num4;
+mod;
+
+let max = 20;
+max;
